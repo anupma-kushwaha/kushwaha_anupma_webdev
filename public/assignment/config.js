cf4594e8ac31d68be5c4d1cf9585d618161d6e5e
anupma-kushwaha <anupma.kushwaha@outlook.com>
2016-12-11 17:51:05 -0500

    more changes

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -22,58 +22,66 @@
                 controllerAs: "model",
                 resolve: {checkLogin: checkLogin}
             })
-
             .when("/user/:uid/website", {
                 templateUrl: "views/website/website-list.view.client.html",
                 controller: "WebsiteListController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
             .when("/user/:uid/website/new", {
                 templateUrl: "views/website/website-new.view.client.html",
                 controller: "NewWebsiteController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
             .when("/user/:uid/website/:wid", {
                 templateUrl: "views/website/website-edit.view.client.html",
                 controller: "EditWebsiteController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
 
             .when("/user/:uid/website/:wid/page", {
                 templateUrl: "views/page/page-list.view.client.html",
                 controller: "PageListController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
             .when("/user/:uid/website/:wid/page/new", {
                 templateUrl: "views/page/page-new.view.client.html",
                 controller: "NewPageController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
             .when("/user/:uid/website/:wid/page/:pid", {
                 templateUrl: "views/page/page-edit.view.client.html",
                 controller: "EditPageController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
-
             .when("/user/:uid/website/:wid/page/:pid/widget", {
                 templateUrl: "views/widget/widget-list.view.client.html",
                 controller: "WidgetListController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
             .when("/user/:uid/website/:wid/page/:pid/widget/new", {
                 templateUrl: "views/widget/widget-chooser.view.client.html",
                 controller: "NewWidgetController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
             .when("/user/:uid/website/:wid/page/:pid/widget/new/:type", {
                 templateUrl: "/assignment/views/widget/widget-new.view.client.html",
                 controller: "NewWidgetController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
             .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
                 templateUrl: "views/widget/widget-edit.view.client.html",
                 controller: "EditWidgetController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve: {checkLogin: checkLogin}
             })
 
             .otherwise({
@@ -93,7 +101,5 @@
                 });
             return deferred.promise;
         }
-
-
     }
 })();
